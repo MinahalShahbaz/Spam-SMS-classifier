f5d8b6cc8cf246a942db586aee2aa8303626ea16
MinahalShahbaz <minahalshahbaz517@gmail.com>
2026-04-03 12:51:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import nltk
 
-# Download punkt tokenizer if missing
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+# Download punkt tokenizer if not already present
+nltk.download('punkt', quiet=True)
+
 import streamlit as st
 import pickle
 import sklearn
@@ -14,7 +12,6 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
-
 
 def transform_text(text):
     text = text.lower()
@@ -50,16 +47,15 @@ st.title("Email/Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
-if st.button('predict'):
+if st.button("Predict"):
+    if input_sms.strip():  # make sure user typed something
+        transformed_sms = transform_text(input_sms)
+        vector_input = vectorizer.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
 
- # 1. preprocess
- transformed_sms = transform_text(input_sms) 
- # 2. vectorize
- vector_input = tfidf.transform([transformed_sms])
- # 3. predict
- result = model.predict(vector_input)[0]
- # 4. Display
- if result == 1:
-     st.header("Spam")
- else:
-     st.header("Spam Button")
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+    else:
+        st.warning("Please enter a message to predict.")
